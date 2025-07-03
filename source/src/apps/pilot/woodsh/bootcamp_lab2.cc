4c7b614f49c7ed98f690498613c4fbd068da953e
woodsh17 <hmwoods14@gmail.com>
2025-07-03 10:07:07 -0500

    Editing bootcamp_lab2 to load in a Pose from a PDB file

--- source/src/apps/pilot/woodsh/bootcamp_lab2.cc
+++ source/src/apps/pilot/woodsh/bootcamp_lab2.cc
@@ -8,8 +8,22 @@
 // (c) addressed to University of Washington CoMotion, email: license@uw.edu.
 
 #include <iostream>
+#include <basic/options/option.hh>
+#include <basic/options/keys/OptionKeys.hh>
+#include <devel/init.hh>
+#include <basic/options/keys/in.OptionKeys.gen.hh>
 
-int main() {
+int main( int argc, char * argv [] )
+{
 	std::cout << "Hello World!" << std::endl;
+	devel::init( argc, argv );
+	utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
+	if ( filenames.size() > 0 ) {
+		std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+	} else {
+		std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
+		return 1;
+	}
 	return 0;
 }
+
